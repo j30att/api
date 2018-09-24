9dd21c6a5372f9b3b2492dfa76b8cbd5d6f95af4
j30att <j30att@gmail.com>
2018-09-24 20:47:26 +0800

    fix views

--- resources/views/events/parts/event.blade.php
+++ resources/views/events/parts/event.blade.php
@@ -4,8 +4,10 @@
                             <img src="/images/event.png" alt="">
                         </div>
                         <div class="event-item__text">
+                            <a href="{{route('event', ['id'=>$event->id])}}">
                             <div class="event-item__title">{{str_limit($event->title, 20)}}</div>
                             <div class="event-item__summa">£{{$event->fund}} GTD</div>
+                            </a>
                             <div class="event-item__date">{{$event->formatted_data}}</div>
                         </div>
 
