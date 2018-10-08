1878dbb9ca797b3bf9cdc113bcea315973b7c860
j30att <j30att@gmail.com>
2018-10-08 17:05:06 +0800

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-        <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/'.$_typeDevice.'/app.js') }}" type="text/javascript"></script>
+        <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
+        <script src="{{ asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <title>Poker</title>
     <script>
