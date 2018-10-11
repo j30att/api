233366d7a9b45d8e60110e4a2b2cf41a911a7f09
j30att <j30att@gmail.com>
2018-10-11 21:36:39 +0800

    profile component

--- resources/js/desktop/Components.js
+++ resources/js/desktop/Components.js
@@ -1,3 +1,4 @@
+import {ProfileComponent} from "./Components/profile/profile.controller";
 import {MainMenuComponent} from "./Components/main-menu/main-menu.controller";
 import {SaleCreateComponent} from "./Components/sale-create/sale-create.controller";
 import {SaleManageComponent} from "./Components/sale-manage/sale-manage.controller";
@@ -7,6 +8,7 @@ let angular = require('angular');
 
 let module = angular.module('Components', []);
 
+module.component('profile', ProfileComponent);
 module.component('mainMenu', MainMenuComponent);
 module.component('saleCreate', SaleCreateComponent);
 module.component('saleManage', SaleManageComponent);
