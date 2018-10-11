3ebb8641adcb6352a38e65754f75fdba42058220
Dmitry Woron <worond@gmail.com>
2018-10-11 22:24:42 +0800

    Added filters

--- resources/js/desktop/Controllers/events/EventsController.js
+++ resources/js/desktop/Controllers/events/EventsController.js
@@ -3,25 +3,57 @@ class EventsController {
         this.$scope = $scope;
         this.$element = $element;
         this.EventsResourceService = EventsResourceService;
-       
+
         this._opts = {dataLoad: false};
 
         this.events = [];
         this.filters = {};
-        this.activeFilter = {};
+        this.activeFilter = [];
 
         this.getEvents();
         this.getFilters();
     }
 
-    $onInit(){
-        this.$element.find('input').on('keydown', function(ev) {
+    $onInit() {
+        this.$element.find('input').on('keydown', function (ev) {
             ev.stopPropagation();
         });
     }
 
     setFilter(param) {
 
+    }
+
+    disableFilter(type, index) {
+        if (type) {
+            if (this.activeFilter[type][index]) {
+                this.activeFilter[type].splice(index, 1);
+            }
+        } else {
+            this.activeFilter = [];
+        }
+    }
+
+    isSetActiveFilters() {
+        let date, event, country, venue;
+
+        if (this.activeFilter.date) {
+            date = true;
+        }
+
+        if (this.activeFilter.event && this.activeFilter.event.length > 0) {
+            event = true;
+        }
+
+        if (this.activeFilter.country && this.activeFilter.country.length > 0) {
+            country = true;
+        }
+
+        if (this.activeFilter.venue) {
+            venue = true;
+        }
+
+        return date || event || country || venue;
     }
 
     getFilters() {
@@ -40,7 +72,7 @@ class EventsController {
             });
     }
 
-    
+
 }
 
 EventsController.$inject = ['$scope', '$element', 'EventsResourceService'];
--- resources/js/desktop/Filters.js
+++ resources/js/desktop/Filters.js
@@ -1,6 +1,9 @@
 import {ucFirst} from "../common/filters/uc-first";
+import {optionFilter} from "../common/filters/option-filter";
 
 let module = angular.module('Filters', []);
+
 module.filter('ucFirst', ucFirst);
+module.filter('optionFilter', optionFilter);
 
 export default module.name;
