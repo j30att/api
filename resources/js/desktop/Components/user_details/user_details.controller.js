cdd86a6effaa1eeb8991cb6f44feca697cdb59b6
elfill <filonenko0406@gmail.com>
2018-11-06 15:45:30 +0800

    add modal window

--- resources/js/desktop/Components/user_details/user_details.controller.js
+++ resources/js/desktop/Components/user_details/user_details.controller.js
@@ -1,10 +1,13 @@
 
 class UserDetails {
-    constructor($scope,SalesResourceService, $mdSidenav, $http, SalesService, $timeout, $state, DealerResourceService) {
+    constructor($scope,SalesResourceService, $mdSidenav, $http, SalesService,
+                $timeout, $state, DealerResourceService, $mdDialog)
+    {
         this.SalesResourceService = SalesResourceService;
         this.DealerResourceService = DealerResourceService;
         this.SalesService = SalesService;
         this.$mdSidenav = $mdSidenav;
+        this.$mdDialog = $mdDialog;
         this.$timeout=$timeout;
         this.$state = $state;
         this.$scope = $scope;
@@ -51,12 +54,72 @@ class UserDetails {
         let eventTitle = this.events[this.key].title;
         return eventTitle;
     }
+    showLosePopUp(){
+        let confirm = this.$mdDialog.confirm()
+            .parent(angular.element(document.querySelector('[md-component-id="right_user_details"]')))
+            .title('Confirm your action')
+            .textContent('Are you sure that ' + this.user.name +' lose this game?\n' +
+                'You can’t be able to change this action later!')
+            .ok('Confirm')
+            .cancel('Cancel');
+        this.$mdDialog.show(confirm).then(() => {
+
+        }, () => {
+        });
+
+        setTimeout(function () {
+            let buttonConf = document.getElementsByClassName('md-confirm-button')[0];
+            buttonConf.setAttribute('style', 'background-color:#ff3700 !important');
+        }, 10);
+
+
+    }
+
+
+    showLeftPopUp(){
+        let confirm = this.$mdDialog.confirm()
+            .parent(angular.element(document.querySelector('[md-component-id="right_user_details"]')))
+            .title('Confirm your action')
+            .textContent('Are you sure that ' + this.user.name +' left this game?\n' +
+                'You can’t be able to change this action later!')
+            .ok('Confirm')
+            .cancel('Cancel');
+
+        this.$mdDialog.show(confirm).then(() => {
+
+        }, () => {
+        });
+
+        setTimeout(function () {
+            let buttonConf = document.getElementsByClassName('md-confirm-button')[0];
+            buttonConf.setAttribute('style', 'background-color:#999999 !important');
+        }, 10);
+    }
+    showWonPopUp(){
+        let confirm = this.$mdDialog.confirm()
+            .parent(angular.element(document.querySelector('[md-component-id="right_user_details"]')))
+            .title('Confirm your action')
+            .textContent('Are you sure that ' + this.user.name +' Won this game?\n' +
+                'You can’t be able to change this action later!')
+            .ok('Confirm')
+            .cancel('Cancel');
+
+        this.$mdDialog.show(confirm).then(() => {
+
+        }, () => {
+        });
+
+        setTimeout(function () {
+            let buttonConf = document.getElementsByClassName('md-confirm-button')[0];
+            buttonConf.setAttribute('style', 'background-color:#00c811 !important');
+        }, 10);
+    }
 
 
 };
 
 UserDetails.$inject = ['$scope', 'SalesResourceService',
-    '$mdSidenav', '$http', 'SalesService', '$timeout', '$state', 'DealerResourceService'];
+    '$mdSidenav', '$http', 'SalesService', '$timeout', '$state', 'DealerResourceService', '$mdDialog'];
 
 export const UserDetailsComponent = {
     bindings: {
