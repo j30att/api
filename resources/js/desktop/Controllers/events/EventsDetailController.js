7e67d4096ea4ea778e4e37057068dfb94b1233fb
Dmitry Woron <worond@gmail.com>
2018-11-23 21:36:20 +0800

    Set bid by highest

--- resources/js/desktop/Controllers/events/EventsDetailController.js
+++ resources/js/desktop/Controllers/events/EventsDetailController.js
@@ -5,6 +5,7 @@ class EventsDetailController {
         this.EventsResourceService = EventsResourceService;
         this.event = {};
         this.sale = {};
+        this.bid = {};
         this.activeEvent = [];
 
         this._opts = {dataLoad: false};
@@ -14,14 +15,19 @@ class EventsDetailController {
         this.getEvent();
     }
 
-    toggleSidenav(sale) {
+    toggleSidenav(sale, bid) {
         this.sale = sale;
+
+        this.bid.markup = bid.markup;
+        this.bid.share = bid.share + '%';
+        this.bid.amount = '$' + bid.amount;
+
         this.$scope.$broadcast('sidenav-open', () => {
             console.log('open sidenav')
         });
     }
 
-    toggleEvent(event){
+    toggleEvent(event) {
         this.activeEvent = event;
     }
 
@@ -33,7 +39,7 @@ class EventsDetailController {
                 .then(response => {
                     this.event = response.data.data;
 
-                    if(this.event.subevents[0]){
+                    if (this.event.subevents[0]) {
                         this.activeEvent = this.event.subevents[0];
                     } else {
                         this.activeEvent = this.event;
